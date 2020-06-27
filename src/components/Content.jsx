e519b1348a4ef39989de513233a28e09deb3529b
Domingo Cabeza <domingoacd1@gmail.com>
2020-06-27 18:52:35 -0400

    Added content, footer componets with styles

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/content.css';
+export default class Testing extends React.Component{
+  render() {
+    return (
+      <div className="content">
+        I am the component!
+      </div>
+    );
+  }
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/footer.css';
+export default class Footer extends React.Component {
+  render() {
+    return(
+      <div className="footer">
+        <h4>Made by Domingo Cabeza</h4>
+      </div>
+    );
+  }
+}
