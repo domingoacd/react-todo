4cdb3295abce2b8354cc49498f6ba97594482d54
Domingo Cabeza <domingoacd1@gmail.com>
2020-06-28 00:21:11 -0400

    Added add list event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import '../css/button.css';
 export default class Button extends React.Component{
 
+  addList() {
+    console.log('clicked');
+  }
+
+  getClickEvent() {
+    if (this.props.type === 'new_list') {
+      return this.addList;
+    }
+  }
+
   render() {
     return (
-      <button className={`btn ${this.props.type}`}>
+      <button className={`btn ${this.props.type}`} onClick={this.getClickEvent()}>
         <span className="plus_icon">+</span>
         Add list
       </button>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,35 @@ import MiniList from '../components/MiniList';
 import '../css/content.css';
 
 export default class Content extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      thereAreNoLists: true,
+      lists: null
+    }
+  }
+  showModal() {
+    
+  }
+
+  componentDidMount () {
+    const savedLists = localStorage.getItem('lists');
+    if (savedLists) {
+      this.setState({thereAreNoLists: false});
+    } else {
+      this.setState({thereAreNoLists: true});
+    }
+  }
+  
+  getAllLists() {
+    return(
+      <div>
+        <MiniList color="blue" name="Homework" amount_of_tasks="4" />
+        <MiniList color="orange" name="Chores" amount_of_tasks="2" />
+      </div>
+    )
+  }
   render() {
     return (
       <div className="content">
@@ -12,8 +41,13 @@ export default class Content extends React.Component{
           <h2>This are your lists</h2>
           <Button type="new_list" />
           <div className="lists_container">
-            <MiniList color="blue" name="Homework" amount_of_tasks="4"/>
-            <MiniList color="orange" name="Chores" amount_of_tasks="2"/>
+            {
+              !this.state.thereAreNoLists ?
+                <p className="no_lists">Oh, there are no lists yet.</p>
+                :
+                this.getAllLists()
+            }
+            
             {/* <p className="no_lists">Oh, there are no lists yet.</p> */}
           </div>
         </div>
