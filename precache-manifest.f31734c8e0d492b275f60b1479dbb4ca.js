7199255bc4c14ba467fc077b3c0c5f86068ce3fe
Domingo Cabeza <domingoacd1@gmail.com>
2020-07-10 17:04:53 -0400

    Updates

---
+++ precache-manifest.f31734c8e0d492b275f60b1479dbb4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd396d85ad5b6551e5ecc9d9db95f4a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0bf577b29a9b3c58b50b",
+    "url": "/react-todo/static/css/main.92126f6a.chunk.css"
+  },
+  {
+    "revision": "04cbc69e137fa393c10a",
+    "url": "/react-todo/static/js/2.b8f350bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.b8f350bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf577b29a9b3c58b50b",
+    "url": "/react-todo/static/js/main.940a11fb.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
